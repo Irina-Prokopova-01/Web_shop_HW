ff4181921bcb067b8a268c688730286b7793aa3d
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-09-28 20:22:51 +0300

    add info in README.md, get_post_request.py

---
+++ src/get_post_request.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """Класс, описывающий работу сервера. Дочерний класс класса BaseHTTPRequestHandler."""
+
+    def do_GET(self):
+        """Метод, обрабатывающий GET запросы."""
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))
+
+    def do_POST(self):
+        """Метод, обрабатывающий POST запросы."""
+        content_length = int(self.headers["Content-Length"])
+        body = self.rfile.read(content_length)
+        print(body)
+        self.send_response(200)
+        self.end_headers()
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server started http://{hostName}:{serverPort}")
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
+    print("Server stopped.")
